000cb49ee95fc2733d10cc658b3ce6595a820eb5
Someone Somewhere <someone167@pm.me>
2022-02-02 18:55:16 +0000

    Add Bitset

---
+++ include/slib/Bitset.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#include <stddef.h>
+#include <stdint.h>
+
+namespace slib {
+    template <const size_t numBits>
+    class Bitset {
+    public:
+        class BitsetRef {
+            friend class Bitset<numBits>;
+            Bitset<numBits>& set;
+            size_t index;
+            BitsetRef(Bitset<numBits>& set, size_t index) : set(set), index(index) {}
+        public:
+            operator bool() {
+                return set.get(index);
+            }
+
+            BitsetRef& operator =(bool b) {
+                set.set(index, b);
+                return *this;
+            }
+        };
+
+        Bitset() {
+            for (size_t i = 0; i < numBytes; i++) {
+                data[i] = 0;
+            }
+        }
+
+        bool get(size_t idx) {
+            size_t dataIdx = idx / typeBits;
+            size_t bitIdx = idx % typeBits;
+            return (bool)((data[dataIdx] >> bitIdx) & 1);
+        }
+
+        void set(size_t idx, bool value) {
+            size_t dataIdx = idx / typeBits;
+            size_t bitIdx = idx % typeBits;
+
+            uint32_t currentVal = data[dataIdx];
+            uint32_t mask = ~(1 << bitIdx);
+
+            data[dataIdx] = (currentVal & mask) | (value << bitIdx);
+        }
+
+        BitsetRef operator[](size_t idx) {
+            return BitsetRef(*this, idx);
+        }
+    private:
+        using DataType = uint8_t;
+        const static uint32_t typeBits = sizeof(DataType) * 8;
+        const static uint32_t numBytes = (numBits + (typeBits - 1)) / typeBits;
+        DataType data[numBytes];
+    };
+}
